c3cbe6c2966e8e16a5471f8524b21686097e2392
Vinícius Araújo Coelho <viniciusaccoelho123@gmail.com>
2026-08-19 01:22:11 -0300

    feat(product): add product repository

---
+++ src/main/java/br/com/viniciusacoelho/invoice_issuance_system/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package br.com.viniciusacoelho.invoice_issuance_system.repository;
+
+import br.com.viniciusacoelho.invoice_issuance_system.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByName(String name);
+
+}
