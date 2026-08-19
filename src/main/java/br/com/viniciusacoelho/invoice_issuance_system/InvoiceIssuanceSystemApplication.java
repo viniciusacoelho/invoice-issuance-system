d6e715be31407474f7b2976513d650822dad58a7
Vinícius Araújo Coelho <viniciusaccoelho123@gmail.com>
2026-08-19 01:09:27 -0300

    chore: initialize project

---
+++ src/main/java/br/com/viniciusacoelho/invoice_issuance_system/InvoiceIssuanceSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.viniciusacoelho.invoice_issuance_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceIssuanceSystemApplication {
+
+    void main() {
+        SpringApplication.run(InvoiceIssuanceSystemApplication.class);
+    }
+
+}
---
+++ src/test/java/br/com/viniciusacoelho/invoice_issuance_system/InvoiceIssuanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viniciusacoelho.invoice_issuance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceIssuanceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
