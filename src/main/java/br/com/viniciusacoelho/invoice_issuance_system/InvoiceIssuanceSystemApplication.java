ec5472f192616e99f18333ace7c2a3cb9a4df921
Vinícius Araújo Coêlho <viniciusaccoelho123@gmail.com>
2026-09-23 01:20:08 -0300

    chore(feign): enable Feign clients

--- src/main/java/br/com/viniciusacoelho/invoice_issuance_system/InvoiceIssuanceSystemApplication.java
+++ src/main/java/br/com/viniciusacoelho/invoice_issuance_system/InvoiceIssuanceSystemApplication.java
@@ -2,8 +2,10 @@ package br.com.viniciusacoelho.invoice_issuance_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class InvoiceIssuanceSystemApplication {
 
     void main() {
